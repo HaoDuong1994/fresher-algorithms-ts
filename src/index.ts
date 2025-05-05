ce2e83f2d9a110e9d6e84eab470f596fbf54c568
HaoDuong1994 <hao.dv5370@gmail.com>
2025-05-05 18:04:16 +0700

    find all index in array

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,29 @@ const findIndexRecursive = <T>(
   if (array[index] == value) return index;
   return findIndexRecursive(array, value, index + 1);
 };
+
+//2) find All index of value
+
+const findAllIndex = <T>(array: T[], value: T): number[] | undefined => {
+  let result: number[] = [];
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] == value) {
+      result.push(i);
+    }
+  }
+  if (result.length == 0) return undefined;
+  return result;
+};
+
+const findAllIndexRecursive = <T>(
+  array: T[],
+  value: T,
+  index: number = 0,
+  currentArrray: number[] = []
+): number[] | undefined => {
+  if (index > array.length && currentArrray.length == 0) return undefined;
+  if (array[index] == value) {
+    currentArrray.push(index);
+    return findAllIndexRecursive(array, value, index + 1, currentArrray);
+  }
+};
