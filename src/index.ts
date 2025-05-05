18791b59b7440011a16040df03643da509dc93a5
HaoDuong1994 <hao.dv5370@gmail.com>
2025-05-05 16:46:41 +0700

    search Algorithms

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,19 @@
-export {};
-const arr = [1, 2, 3, 4];
-// tìm phần tử lớn nhứt
-const checkMax = (array: number[]): number => {
-  let a = 0;
-  array.forEach((item) => {
-    if (item > a) {
-      a = item;
-    } else {
-      a = a;
-    }
-  });
-  return a;
-};
-console.log("chieu dai mang", arr.length);
-const reverseArray = <T>(a: T[]): T[] => {
-  let originArray: T[] = [];
-  a.forEach((item, index) => {
-    console.log("index", index);
-    const lastIndex = a.length - index - 1;
-    originArray.push(a[lastIndex]);
-  });
-  return originArray;
-};
-
-declare global {
-  interface Array<T> {
-    myReverse(): T[];
+//Search Algorithms
+// 1) Find value in Array
+const findIndex = <T>(array: T[], value: T): number | undefined => {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i] == value);
+    if (array[i] == value) return i;
   }
-}
-Array.prototype.myReverse = function <T>(): T[] {
-  let originArray: T[] = [];
-  this.forEach((item: number, index: number) => {
-    console.log("index", index);
-    const lastIndex: number = this.length - index - 1;
-    originArray.push(this[lastIndex]);
-  });
-  return originArray;
+  return undefined;
 };
 
-const secArrray = [3, 4, 5, 6];
-const newArray: number[] = secArrray.myReverse();
-console.log(newArray);
-// 5 = 5x4x3x2x1
-const myFlatoral = (a: number) => {
-  let value: number;
-  value = a * (a - 1);
-  return value;
+const findIndexRecursive = <T>(
+  array: T[],
+  value: T,
+  index: number = 0
+): number | undefined => {
+  if (index >= array.length && array[index] != value) return undefined;
+  if (array[index] == value) return index;
+  return findIndexRecursive(array, value, index + 1);
 };
-
-const nCount = (n: number): void => {
-  if (n == 1) return console.log(1);
-  console.log(n);
-  nCount(n - 1);
-};
-
-nCount(5);
-// sum5 = 5+4+3+2+1
-const sumN = (n: number): number => {
-  if (n == 1) return 1;
-  return n + sumN(n - 1);
-};
-//f5 = f4 + f3
-const value = sumN(5);
-console.log(value);
-const fibN = (n: number): number => {
-  if (n == 1) return 1;
-  if (n == 0) return 0;
-  return fibN(n - 1) + fibN(n - 2);
-};
-
-//giai thừa 5 = 5*4*3
-
-const factorialN = (n: number): number => {
-  if (n == 1) return 1;
-  return n * factorialN(n - 1);
-};
-
-const value1 = factorialN(5);
-console.log(value1);
